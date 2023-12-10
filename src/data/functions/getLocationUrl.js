db0d141a360d95908b5691cf80088b1e77772d78
unknown <talmoshel444@gmail.com>
2023-12-10 19:49:31 +0200

    fixBug-all-bonuses-applied

--- src/data/functions/getLocationUrl.js
+++ src/data/functions/getLocationUrl.js
@@ -6,7 +6,7 @@ export function getLocationUrl(location, isGeoApi) {
             &q=${location}`
             return url
         }
-            url = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=kLBFG7ks0fYmfw5znsZ5QYEkkgZbHsjh&q=${location.coordinates.lat}%2C${location.coordinates.lng}`
+            url = `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=kLBFG7ks0fYmfw5znsZ5QYEkkgZbHsjh&q=${location.coordinates.lat}%2C${location.coordinates.lng}`
             return url
     
 }
